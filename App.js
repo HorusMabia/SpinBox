7220c466e6f031f01894d4695657da183d968d62
HorusMabia <horus.mabia@gmail.com>
2024-07-07 20:30:02 +0100

    react three fiber (not working)

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber/native'
+import { useGLTF } from '@react-three/drei/native'
+import modelPath from './path/to/model.glb'
+
+function Model(props) {
+  const gltf = useGLTF(modelPath)
+  return <primitive {...props} object={gltf.scene} />
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Works on everything ! Web ! Android ! iOS !</Text>
+      <Text>Horus Mabia</Text>
       <StatusBar style="auto" />
+      <Canvas>
+        <ambientLight />
+        <Suspense>
+          <Model />
+        </Suspense>
+    </Canvas>
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },  
 });
